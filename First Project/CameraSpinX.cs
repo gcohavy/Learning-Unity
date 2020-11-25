7562d0962e19202e95ae2384a79df7151f8dde2e
Gabriel Cohavy <gcohavy@gmail.com>
2020-11-25 14:16:50 -0800

    First Challenge complete

---
+++ First Project/CameraSpinX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpinX : MonoBehaviour
+{
+    //Declare Variables
+    public float propellorSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spin the propellor based on the propellor speed
+        transform.Rotate(Vector3.right, Time.deltaTime * propellorSpeed);
+    }
+}
