9da440ea46aee291425e2aebbcba8f5b423847b3
Gabriel Cohavy <gcohavy@gmail.com>
2020-12-04 09:53:39 -0800

    added scripts for top down shooter

---
+++ Top Down Shooter/DestroyOutOfBounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float bottomBound = -10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Destroy objects that leave the screen
+        if( transform.position.z > topBound )
+        {
+            Destroy(gameObject);
+        } else if ( transform.position.z < bottomBound )
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Top Down Shooter/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] spawnPrefabs;
+    private float spawnRangeX = 12.0f;
+    private float spawnRangeZ = 15.0f;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),1,spawnRangeZ);
+        int spawnIndex = Random.Range(0, spawnPrefabs.Length);
+
+        Instantiate(spawnPrefabs[spawnIndex], spawnPos, spawnPrefabs[spawnIndex].transform.rotation);
+    }
+}
