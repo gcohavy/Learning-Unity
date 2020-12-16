c52c148f4d3afce36dcb82b0771dcc90f74dea7d
Gabriel Cohavy <gcohavy@gmail.com>
2020-12-16 11:22:30 -0800

    Changed top down shooter to implement Object Pooling for better optimization

--- Top Down Shooter/DestroyOutOfBounds.cs
+++ Top Down Shooter/DestroyOutOfBounds.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float bottomBound = -10.0f;
+    private float topBound = 30;
+    private float lowerBound = -10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Destroy objects that leave the screen
-        if( transform.position.z > topBound )
+        if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
-        } else if ( transform.position.z < bottomBound )
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
+        }
+        else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+
     }
 }
--- Top Down Shooter/PlayerController.cs
+++ Top Down Shooter/PlayerController.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 15.0f;
-    public float xRange = 12.0f;
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float xRange = 20;
+    public GameObject projectilePrefab;
 
-    public GameObject ProjectilePrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -xRange) 
+        // Check for left and right bounds
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if(transform.position.x > xRange) 
+
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        // Player movement left to right
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Instantiate a projectile from the player
-            Instantiate(ProjectilePrefab, transform.position + new Vector3(0,0,1.5f), ProjectilePrefab.transform.rotation);
+            // No longer necessary to Instantiate prefabs
+            // Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+            // Get an object object from the pool
+            GameObject pooledProjectile = ObjectPooler.SharedInstance.GetPooledObject();
+            if (pooledProjectile != null)
+            {
+                pooledProjectile.SetActive(true); // activate it
+                pooledProjectile.transform.position = transform.position; // position it at player
+            }
         }
+
+
+
     }
 }
